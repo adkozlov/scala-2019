05a3c0df4bf7c64bd25ab29f6db371ca7db03ebf
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2019-11-06 12:02:02 +0300

    lecture 1 added

---
+++ src/main/scala/ru/spbau/jvm/scala/Foo.scala
@@ -0,0 +1,5 @@
+package ru.spbau.jvm.scala
+
+trait Foo {
+  def foo: Int
+}
---
+++ src/main/scala/ru/spbau/jvm/scala/Main.scala
@@ -0,0 +1,9 @@
+package ru.spbau.jvm.scala
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val foo: Foo = Bar()
+    println(foo.foo)
+  }
+}
