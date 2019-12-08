0ed8e55f049db18cce70a89ad032fa53fd9027af
Vladislav Tankov <vdtankov@gmail.com>
2019-12-08 21:11:49 +0300

    Implement task 2

---
+++ src/main/scala/ru/spbau/jvm/scala/avl/MultiSet.scala
@@ -0,0 +1,215 @@
+package ru.spbau.jvm.scala.avl
+
+
+import scala.util.Random
+
+class MultiSet[T]()(implicit ord: T => Ordered[T]) {
+  private var myHead: Node[T] = _
+  private var mySize: Int = 0
+
+  //Implement Treap, as one of the simplest
+  case class Node[E](key: E,
+                     var total: Int = 1, balancingKey: Int = Random.nextInt(),
+                     //Tried option here, but it looked even the worse
+                     var left: Node[E] = null, var right: Node[E] = null)(implicit ord: E => Ordered[E]) {
+    def get(key: E): Node[E] = {
+      if (this.key == key) return this
+
+      if (this.key < key) {
+        if (right != null) {
+          right.get(key)
+        } else {
+          null
+        }
+      } else {
+        if (left != null) {
+          left.get(key)
+        } else {
+          null
+        }
+      }
+    }
+
+
+    def add(key: E): Node[E] = {
+      if (this.key == key) {
+        total += 1
+        return this
+      }
+
+      if (key < this.key) {
+        left = if (left == null) {
+          new Node[E](key)
+        } else {
+          left.add(key)
+        }
+
+        if (left.balancingKey > balancingKey) return rightPivot()
+      } else if (key > this.key) {
+        right = if (right == null) {
+          new Node[E](key)
+        } else {
+          right.add(key)
+        }
+
+        if (right.balancingKey > balancingKey) return leftPivot()
+      }
+
+      this
+    }
+
+    def remove(key: E): Node[E] = {
+      if (key == this.key) {
+        if (total > 1) {
+          total -= 1
+        } else {
+          if (left == null) return right
+          if (right == null) return left
+
+          if (left.balancingKey < right.balancingKey) {
+            val removeFrom = leftPivot()
+            removeFrom.left = removeFrom.left.remove(key)
+            return removeFrom
+          } else {
+            val removeFrom = rightPivot()
+            removeFrom.right = removeFrom.right.remove(key)
+            return removeFrom
+          }
+        }
+      }
+
+      if (left != null && key < this.key) {
+        left = left.remove(key)
+      } else if (right != null && key > this.key) {
+        right = right.remove(key)
+      }
+
+      this
+    }
+
+    private def rightPivot(): Node[E] = {
+      val w = this.left
+      val u = w.right
+
+      this.left = u
+
+      w.right = this
+
+      w
+    }
+
+    private def leftPivot(): Node[E] = {
+      val w = this.right
+      val u = w.left
+
+      this.right = u
+
+      w.left = this
+
+      w
+    }
+
+    def toList: List[(E, Int)] = {
+      if (this.left != null && this.right != null) {
+        this.left.toList ++ List((this.key, this.total)) ++ this.right.toList
+      } else if (this.left != null) {
+        this.left.toList ++ List((this.key, this.total))
+      } else if (this.right != null) {
+        List((this.key, this.total)) ++ this.right.toList
+      } else {
+        List((this.key, this.total))
+      }
+    }
+
+    def iterator(): Iterator[(E, Int)] = toList.iterator
+  }
+
+  def add(e: T)(implicit ord: T => Ordered[T]): MultiSet[T] = {
+    myHead = if (myHead == null) {
+      Node(e)
+    } else {
+      myHead.add(e)
+    }
+
+    mySize += 1
+    this
+  }
+
+  def contains(key: T): Boolean = if (myHead != null) {
+    myHead.get(key) != null
+  } else {
+    false
+  }
+
+
+  def remove(o: T): MultiSet[T] = {
+    if (myHead != null && contains(o)) {
+      myHead = myHead.remove(o)
+      mySize -= 1;
+    }
+    this
+  }
+
+  def map[U](function: T => U)(implicit ord: U => Ordered[U]): MultiSet[U] = MultiSet(toList.map(function))
+
+  def filter(predicate: T => Boolean)(implicit ord: T => Ordered[T]): MultiSet[T] = MultiSet(toList.filter(predicate))
+
+  def withFilter(predicate: T => Boolean): MultiSet[T] = filter(predicate)
+
+  def foreach[U](function: T => U): Unit = toList.foreach(function)
+
+  def flatMap[U](f: T => IterableOnce[U])(implicit ord: Ordering[U]): MultiSet[U] = {
+    MultiSet(toList.flatMap(f))
+  }
+
+  //Sum by whole sum, not minimum
+  def |(other: MultiSet[T]): MultiSet[T] = {
+    MultiSet(toList ++ other.toList)
+  }
+
+  //Intersection by keys
+  def &(other: MultiSet[T]): MultiSet[T] = {
+    MultiSet(toList.filter(x => other.toList.contains(x)) ++ other.toList.filter(x => toList.contains(x)))
+  }
+
+  def apply(key: T): Int = if (myHead != null) {
+    val node = myHead.get(key)
+    if (node != null) {
+      node.total
+    } else {
+      0
+    }
+  } else {
+    0
+  }
+
+  override def toString: String = if (myHead != null) {
+    myHead.toList.map(node => s"${node._1} -> ${node._2}").mkString("[", ", ", "]")
+  } else {
+    "[]"
+  }
+
+  private def toList: List[T] = if (myHead != null) {
+    myHead.toList.flatMap(value => List.range(1, value._2 + 1).map(_ => value._1))
+  } else {
+    List.empty
+  }
+
+  def iterator(): Iterator[T] = toList.iterator
+
+  def size(): Int = mySize
+}
+
+object MultiSet {
+  def apply[U]()(implicit ord: U => Ordered[U]) = new MultiSet[U]()
+
+  def apply[U](values: U*)(implicit ord: U => Ordered[U]): MultiSet[U] = MultiSet[U](values.toList)
+
+  def apply[U](values: List[U])(implicit ord: U => Ordered[U]): MultiSet[U] = {
+    val result = new MultiSet[U]
+    values.foreach(result.add)
+    result
+  }
+
+  def empty[U](implicit ord: U => Ordered[U]): MultiSet[U] = MultiSet[U]()
+}
---
+++ src/test/scala/ru/spbau/jvm/scala/avl/MultiSetTest.scala
@@ -0,0 +1,40 @@
+package ru.spbau.jvm.scala.avl
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class MultiSetTest extends FlatSpec with Matchers {
+  private def testSet1() = MultiSet[Int](1, 2, 3, 4, 5, 5, 9, 9)
+  private def testSet2() = MultiSet[Int](4, 5, 6, 6, 9)
+
+  "Constructor" should "be correct" in {
+    testSet1().toString should be("[1 -> 1, 2 -> 1, 3 -> 1, 4 -> 1, 5 -> 2, 9 -> 2]")
+    MultiSet[Int](List.empty).toString should be("[]")
+  }
+
+  "add" should "be correct" in {
+    MultiSet.empty[Int].add(0).toString should be("[0 -> 1]")
+    MultiSet.empty[Int].add(-1).add(0).add(1).add(12).add(1).toString should be("[-1 -> 1, 0 -> 1, 1 -> 2, 12 -> 1]")
+  }
+
+  "remove" should "be correct" in {
+    MultiSet.empty[Int].add(0).remove(0).toString should be("[]")
+    MultiSet.empty[Int].add(-1).add(0).add(1).add(12).add(1).remove(1).add(12).toString should be("[-1 -> 1, 0 -> 1, 1 -> 1, 12 -> 2]")
+  }
+
+  "size" should "be correct" in {
+    MultiSet.empty[Int].add(0).remove(0).size() should be(0)
+    MultiSet.empty[Int].add(-1).add(0).add(1).add(12).add(1).remove(1).add(12).size() should be(5)
+  }
+
+  "&" should "be correct" in {
+    (testSet1() & testSet2()).toString should be("[4 -> 2, 5 -> 3, 9 -> 3]")
+  }
+
+  "|" should "be correct" in {
+    (testSet1() | testSet2()).toString should be("[1 -> 1, 2 -> 1, 3 -> 1, 4 -> 2, 5 -> 3, 6 -> 2, 9 -> 3]")
+  }
+
+  "map" should "be correct" in {
+    testSet1().map(it => -it).toString should be("[-9 -> 2, -5 -> 2, -4 -> 1, -3 -> 1, -2 -> 1, -1 -> 1]")
+  }
+}
