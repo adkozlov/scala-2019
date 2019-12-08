19afb3bc57a13c74b315886c65671561f0683997
Maxim Zuev <max-zu99@mail.ru>
2019-12-08 15:56:18 +0300

    hw done

---
+++ src/main/scala/ru/spbau/jvm/scala/multiset/MultiSet.scala
@@ -0,0 +1,244 @@
+package ru.spbau.jvm.scala.multiset
+
+import java.util.NoSuchElementException
+
+case class DataKey[A](key: A, var count: Int = 1)
+
+class MultiSet[A](implicit val ordering: Ordering[A]) extends Iterable[A] {
+  type Data = DataKey[A]
+
+  private var root: Option[Node[Data]] = None
+
+  def findData(key: A): Option[Data] = findNode(key) match {
+    case Some(node) => Some(node.value)
+    case None => None
+  }
+
+  def contains(key: A): Boolean = findData(key).isDefined
+
+  def remove(key: A): Unit = findNode(key) match {
+    case Some(node) =>
+      node.value.count -= 1
+      if (node.value.count == 0)
+        root = Node.deleteNode(node)
+    case None =>
+  }
+
+  def add(key: A, count: Int = 1): Unit = {
+    if (count < 1) {
+      throw new IllegalArgumentException
+    }
+    root = Some(addNode(DataKey(key, count)))
+  }
+
+  def getCount(key: A): Int = {
+    findData(key) match {
+      case Some(data) => data.count
+      case None => 0
+    }
+  }
+
+  def apply(key: A): Int = getCount(key)
+
+  def &(other: MultiSet[A]): MultiSet[A] = {
+    val newMS = new MultiSet[A]()
+    val it1 = dataIterator()
+    val it2 = other.dataIterator()
+    while (it1.hasNext && it2.hasNext) {
+      val data1 = it1.getNext
+      val data2 = it2.getNext
+      ordering.compare(data1.key, data2.key) match {
+        case 0 =>
+          val minCount = scala.math.min(data1.count, data2.count)
+          newMS.add(data1.key, minCount)
+          it1.next
+          it2.next
+        case x if x < 0 => it1.next
+        case _ => it2.next
+      }
+    }
+    newMS
+  }
+
+  def |(other: MultiSet[A]): MultiSet[A] = {
+    val newMS = new MultiSet[A]()
+    val it1 = dataIterator()
+    val it2 = other.dataIterator()
+    while (it1.hasNext && it2.hasNext) {
+      val data1 = it1.getNext
+      val data2 = it2.getNext
+      ordering.compare(data1.key, data2.key) match {
+        case 0 =>
+          val sumCount = data1.count + data2.count
+          newMS.add(data1.key, sumCount)
+          it1.next
+          it2.next
+        case x if x < 0 =>
+          newMS.add(data1.key, data1.count)
+          it1.next
+        case _ =>
+          newMS.add(data2.key, data2.count)
+          it2.next
+      }
+    }
+
+    while (it1.hasNext) {
+      val data = it1.next
+      newMS.add(data.key, data.count)
+    }
+    while (it2.hasNext) {
+      val data = it2.next
+      newMS.add(data.key, data.count)
+    }
+    newMS
+  }
+
+  override def toString: _root_.java.lang.String = {
+    val builder = new StringBuilder
+    builder.append('[')
+    val it = dataIterator()
+    while (it.hasNext) {
+      val data = it.next()
+      builder.append(s"${data.key}->${data.count}")
+      if (it.hasNext) builder.append(',')
+    }
+    builder.append(']')
+    builder.toString()
+  }
+
+  def show(tab: String = "", node: Option[Node[Data]] = root): Unit = node match {
+    case Some(node) =>
+      val data = node.value
+      println(s"$tab(${data.key}, ${data.count})")
+      show(tab + "|l----", node.left)
+      show(tab + "|r----", node.right)
+    case None =>
+  }
+
+  override def foreach[U](f: A => U): Unit = {
+    val it = iterator()
+    while (it.hasNext) {
+      f(it.next())
+    }
+  }
+
+  def map[B](f: A => B)(implicit ordering: Ordering[B]): Iterable[B] = {
+    val newMS = MultiSet[B](ordering)
+    val it = iterator()
+    while (it.hasNext) {
+      newMS.add(f(it.next()))
+    }
+    newMS
+  }
+
+  override def filter(pred: A => Boolean): Iterable[A] = {
+    val newMS = MultiSet[A]
+    val it = iterator()
+    while (it.hasNext) {
+      val data = it.next()
+      if (pred(data)) {
+        newMS.add(data)
+      }
+    }
+    newMS
+  }
+
+  private def dataIterator(): MultiSetDataIterator = new MultiSetDataIterator
+
+  class MultiSetDataIterator extends Iterator[DataKey[A]] {
+    private var node = findMinNode()
+
+    override def hasNext: Boolean = node.isDefined
+
+    override def next: DataKey[A] = node match {
+      case Some(_) =>
+        val tmp = node.get.value
+        node = Node.nextNode(node.get)
+        tmp
+      case None => throw new NoSuchElementException
+    }
+
+    def getNext: DataKey[A] = node match {
+      case Some(_) => node.get.value
+      case None => throw new NoSuchElementException
+    }
+  }
+
+  override def iterator(): MultiSetIterator = new MultiSetIterator
+
+  class MultiSetIterator extends Iterator[A] {
+    private val it = new MultiSetDataIterator
+    private var count: Int = if (it.hasNext) it.getNext.count else 0
+
+    override def hasNext: Boolean = count > 0
+
+    override def next(): A = {
+      if (count <= 0) {
+        throw new NoSuchElementException
+      }
+      count -= 1
+      if (count > 0) {
+        it.getNext.key
+      } else {
+        val ret = it.next.key
+        count = if (it.hasNext) it.getNext.count else 0
+        ret
+      }
+    }
+  }
+
+  private def addNode(data: Data): Node[Data] = {
+    findNodeOrParent(data.key) match {
+      case Some(node) =>
+        val compareResult = ordering.compare(data.key, node.value.key)
+        if (compareResult == 0) {
+          node.value.count += data.count
+          root.get
+        } else {
+          val newNode = new Node(data)
+          Node.setSon(Some(node), Some(newNode), compareResult > 0)
+          node.cascadingBalance()
+        }
+      case None =>
+        new Node(data)
+    }
+  }
+
+  private def findNodeOrParent(key: A): Option[Node[Data]] = {
+    var node = root
+    var parent: Option[Node[Data]] = None
+    while (node.isDefined) {
+      parent = node
+      val compareResult = ordering.compare(key, node.get.value.key)
+      compareResult match {
+        case 0 => return node
+        case x if x < 0 => node = node.get.left
+        case _ => node = node.get.right
+      }
+    }
+    parent
+  }
+
+  private def findNode(key: A): Option[Node[Data]] = findNodeOrParent(key) match {
+    case Some(node) => if (ordering.compare(node.value.key, key) == 0) Some(node) else None
+    case None => None
+  }
+
+  private def findMinNode(): Option[Node[Data]] = {
+    var node = root
+    while (node.isDefined && node.get.left.isDefined) {
+      node = node.get.left
+    }
+    node
+  }
+}
+
+object MultiSet {
+  def apply[A](implicit ordering: Ordering[A]) = new MultiSet[A]
+
+  def apply[A](values: A*)(implicit ordering: Ordering[A]): MultiSet[A] = {
+    val m = MultiSet[A](ordering)
+    values.foreach(x => m.add(x))
+    m
+  }
+}
---
+++ src/main/scala/ru/spbau/jvm/scala/multiset/Node.scala
@@ -0,0 +1,148 @@
+package ru.spbau.jvm.scala.multiset
+
+import scala.math.max
+
+class Node[A](var value: A) {
+  private var height = 1
+  var left: Option[Node[A]] = None
+  var right: Option[Node[A]] = None
+  var parent: Option[Node[A]] = None
+
+  private def diff(): Int = Node.height(left) - Node.height(right)
+
+  private def rotate(isRight: Boolean): Node[A] = {
+    val tmpNode = if (isRight) left else right
+    val tmpSon = if (isRight) tmpNode.get.right else tmpNode.get.left
+    Node.changeSon(parent, tmpNode, this)
+    Node.setSon(Some(this), tmpSon, !isRight)
+    Node.setSon(tmpNode, Some(this), isRight)
+    Node.update(this)
+    Node.update(tmpNode)
+    tmpNode.get
+  }
+
+  private def rotateRight(): Node[A] = rotate(true)
+
+  private def rotateLeft(): Node[A] = rotate(false)
+
+  private def bigRotateRight(): Node[A] = {
+    left = Some(left.get.rotateLeft())
+    rotateRight()
+  }
+
+  private def bigRotateLeft(): Node[A] = {
+    right = Some(right.get.rotateRight())
+    rotateLeft()
+  }
+
+  def balance(): Node[A] = {
+    Node.update(this)
+    val balance = diff()
+    if (balance > 1) {
+      if (left.get.diff() > 0)
+        return rotateRight()
+      else
+        return bigRotateRight()
+    }
+    if (balance < -1) {
+      if (right.get.diff() > 0)
+        return bigRotateLeft()
+      else
+        return rotateLeft()
+    }
+    this
+  }
+
+  def cascadingBalance(): Node[A] = {
+    var tmp: Option[Node[A]] = Some(this)
+    while (tmp.get.parent.isDefined) {
+      tmp = tmp.get.balance().parent
+    }
+    tmp.get.balance()
+  }
+}
+
+private object Node {
+
+  private def setParent[A](parent: Option[Node[A]],
+                           son: Option[Node[A]]): Unit = {
+    son match {
+      case Some(node) => node.parent = parent
+      case None =>
+    }
+  }
+
+  def setSon[A](parent: Option[Node[A]],
+                son: Option[Node[A]],
+                isRight: Boolean): Unit = {
+    parent match {
+      case Some(node) =>
+        if (isRight)
+          node.right = son
+        else
+          node.left = son
+      case None =>
+    }
+    setParent(parent, son)
+  }
+
+  def changeSon[A](parent: Option[Node[A]],
+                   son: Option[Node[A]],
+                   oldSon: Node[A]): Unit = {
+    parent match {
+      case Some(node) =>
+        val isRight = node.right.isDefined && node.right.get.eq(oldSon)
+        Node.setSon(parent, son, isRight)
+      case None => setParent(parent, son)
+    }
+  }
+
+  private def height[A](node: Option[Node[A]]): Int = node match {
+    case Some(node) => node.height
+    case None => 0
+  }
+
+  private def update[A](node: Option[Node[A]]): Unit = node match {
+    case Some(node) => update(node)
+    case None =>
+  }
+
+  private def update[A](node: Node[A]): Unit = {
+    node.height = 1 + max(height(node.left), height(node.right))
+  }
+
+  def nextNode[A](node: Node[A]): Option[Node[A]] = {
+    node.right match {
+      case Some(node) =>
+        var currentNode = node
+        while (currentNode.left.isDefined) {
+          currentNode = currentNode.left.get
+        }
+        Some(currentNode)
+      case None =>
+        var currentNode = node
+        while (currentNode.parent.isDefined
+          && currentNode.parent.get.right.isDefined
+          && currentNode.parent.get.right.get.eq(currentNode)) {
+          currentNode = currentNode.parent.get
+        }
+        currentNode.parent
+    }
+  }
+
+  def deleteNode[A](delNode: Node[A]): Option[Node[A]] = {
+    var node: Node[A] = delNode
+    if (node.left.isDefined && node.right.isDefined) {
+      val next = Node.nextNode(node).get
+      node.value = next.value
+      node = next
+    }
+
+    val son = if (node.left.isDefined) node.left else node.right
+    Node.changeSon(node.parent, son, node)
+    node.parent match {
+      case Some(node) => Some(node.cascadingBalance())
+      case None => son
+    }
+  }
+}
