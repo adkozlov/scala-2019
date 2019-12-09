f3888ee048abf43baf1572e82fa9248cebe7ca91
Konstantin Lyubort <lyubortk@gmail.com>
2019-12-09 10:32:20 +0300

    rename types

--- src/main/scala/ru/spbau/jvm/scala/MultiSet.scala
+++ src/main/scala/ru/spbau/jvm/scala/MultiSet.scala
@@ -2,15 +2,15 @@ package ru.spbau.jvm.scala
 
 import java.util.StringJoiner
 
-import ru.spbau.jvm.scala.MultiSet.N.ParentHeight
-import ru.spbau.jvm.scala.MultiSet.{T, _}
+import ru.spbau.jvm.scala.MultiSet.Node.ParentHeight
+import ru.spbau.jvm.scala.MultiSet.{Tree, _}
 
 import scala.math.max
 
 /**
  * Functional persistent MultiSet which is implemented as AVL-tree.
  */
-final class MultiSet[A] private(private val root: N[A], val size: Int)(implicit ord: Ordering[A]) {
+final class MultiSet[A] private(private val root: Node[A], val size: Int)(implicit ord: Ordering[A]) {
 
   outer =>
 
@@ -142,86 +142,86 @@ final class MultiSet[A] private(private val root: N[A], val size: Int)(implicit 
       sj.add(s"$element -> $cnt")
   }.toString
 
-  private def addImpl(node: N[A], element: A, n: Int): N[A] = node match {
-    case Nil => T(Nil, element -> n, Nil)
-    case T(l, e -> cnt, r) if e > element =>
+  private def addImpl(node: Node[A], element: A, n: Int): Node[A] = node match {
+    case Nil => Tree(Nil, element -> n, Nil)
+    case Tree(l, e -> cnt, r) if e > element =>
       val left = addImpl(l, element, n)
-      balance(T(left, e -> cnt, r))
-    case T(l, e -> cnt, r) if e < element =>
+      balance(Tree(left, e -> cnt, r))
+    case Tree(l, e -> cnt, r) if e < element =>
       val right = addImpl(r, element, n)
-      balance(T(l, e -> cnt, right))
-    case T(l, e -> cnt, r) => T(l, e -> (cnt + n), r)
+      balance(Tree(l, e -> cnt, right))
+    case Tree(l, e -> cnt, r) => Tree(l, e -> (cnt + n), r)
   }
 
-  private def delImpl(node: N[A], element: A, n: Int): (N[A], Boolean) = node match {
+  private def delImpl(node: Node[A], element: A, n: Int): (Node[A], Boolean) = node match {
     case Nil => (Nil, false)
-    case T(l, e -> cnt, r) if e > element =>
+    case Tree(l, e -> cnt, r) if e > element =>
       val (left, deleted) = delImpl(l, element, n)
-      balance(T(left, e -> cnt, r)) -> deleted
-    case T(l, e -> cnt, r) if e < element =>
+      balance(Tree(left, e -> cnt, r)) -> deleted
+    case Tree(l, e -> cnt, r) if e < element =>
       val (right, deleted) = delImpl(r, element, n)
-      balance(T(l, e -> cnt, right)) -> deleted
+      balance(Tree(l, e -> cnt, right)) -> deleted
     // e == element
-    case T(l, _ -> cnt, Nil) if cnt <= n => l -> true
-    case T(l, _ -> cnt, r: T[A]) if cnt <= n =>
+    case Tree(l, _ -> cnt, Nil) if cnt <= n => l -> true
+    case Tree(l, _ -> cnt, r: Tree[A]) if cnt <= n =>
       val (replacement, replacementCnt) = leftmost(r)
       val (right, _) = delImpl(r, replacement, replacementCnt)
-      balance(T(l, replacement -> replacementCnt, right)) -> true
+      balance(Tree(l, replacement -> replacementCnt, right)) -> true
     // e == element && cnt > n
-    case T(l, e -> cnt, r) => T(l, e -> (cnt - n), r) -> true
+    case Tree(l, e -> cnt, r) => Tree(l, e -> (cnt - n), r) -> true
   }
 
   @scala.annotation.tailrec
-  private def leftmost(tree: T[A]): (A, Int) = tree match {
-    case T(Nil, value, _) => value
-    case T(l: T[A], _, _) => leftmost(l)
+  private def leftmost(tree: Tree[A]): (A, Int) = tree match {
+    case Tree(Nil, value, _) => value
+    case Tree(l: Tree[A], _, _) => leftmost(l)
   }
 
   @scala.annotation.tailrec
-  private def countImpl(node: N[A], element: A): Int = node match {
+  private def countImpl(node: Node[A], element: A): Int = node match {
     case Nil => 0
-    case T(_, e -> cnt, _) if e == element => cnt
-    case T(l, e -> _, _) if e > element => countImpl(l, element)
-    case T(_, e -> _, r) if e < element => countImpl(r, element)
+    case Tree(_, e -> cnt, _) if e == element => cnt
+    case Tree(l, e -> _, _) if e > element => countImpl(l, element)
+    case Tree(_, e -> _, r) if e < element => countImpl(r, element)
   }
 
-  private def balance(node: T[A]): T[A] = {
+  private def balance(node: Tree[A]): Tree[A] = {
     implicit val parentHeight: ParentHeight = new ParentHeight {
       override val h: Int = node.height
     }
 
     node match {
-      case T(T(N(1), _, N(0)), _, N(0)) => rotateLeftLeft(node)
-      case T(T(N(0), _, N(1)), _, N(0)) => rotateLeftRight(node)
-      case T(N(0), _, T(N(0), _, N(1))) => rotateRightRight(node)
-      case T(N(0), _, T(N(1), _, N(0))) => rotateRightLeft(node)
+      case Tree(Tree(Node(1), _, Node(0)), _, Node(0)) => rotateLeftLeft(node)
+      case Tree(Tree(Node(0), _, Node(1)), _, Node(0)) => rotateLeftRight(node)
+      case Tree(Node(0), _, Tree(Node(0), _, Node(1))) => rotateRightRight(node)
+      case Tree(Node(0), _, Tree(Node(1), _, Node(0))) => rotateRightLeft(node)
       case _ => node
     }
   }
 
-  private def rotateLeftLeft(node: T[A]): T[A] = {
-    val T(T(T(t1, x, t2), y, t3), z, t4) = node
-    T(T(t1, x, t2), y, T(t3, z, t4))
+  private def rotateLeftLeft(node: Tree[A]): Tree[A] = {
+    val Tree(Tree(Tree(t1, x, t2), y, t3), z, t4) = node
+    Tree(Tree(t1, x, t2), y, Tree(t3, z, t4))
   }
 
-  private def rotateLeftRight(node: T[A]): T[A] = {
-    val T(T(t1, y, T(t2, x, t3)), z, t4) = node
-    T(T(t1, y, t2), x, T(t3, z, t4))
+  private def rotateLeftRight(node: Tree[A]): Tree[A] = {
+    val Tree(Tree(t1, y, Tree(t2, x, t3)), z, t4) = node
+    Tree(Tree(t1, y, t2), x, Tree(t3, z, t4))
   }
 
-  private def rotateRightRight(node: T[A]): T[A] = {
-    val T(t1, z, T(t2, y, T(t3, x, t4))) = node
-    T(T(t1, z, t2), y, T(t3, x, t4))
+  private def rotateRightRight(node: Tree[A]): Tree[A] = {
+    val Tree(t1, z, Tree(t2, y, Tree(t3, x, t4))) = node
+    Tree(Tree(t1, z, t2), y, Tree(t3, x, t4))
   }
 
-  private def rotateRightLeft(node: T[A]): T[A] = {
-    val T(t1, z, T(T(t2, x, t3), y, t4)) = node
-    T(T(t1, z, t2), x, T(t3, y, t4))
+  private def rotateRightLeft(node: Tree[A]): Tree[A] = {
+    val Tree(t1, z, Tree(Tree(t2, x, t3), y, t4)) = node
+    Tree(Tree(t1, z, t2), x, Tree(t3, y, t4))
   }
 
-  private def foldLeftImpl[B](container: B, root: N[A])(op: (B, (A, Int)) => B): B = root match {
+  private def foldLeftImpl[B](container: B, root: Node[A])(op: (B, (A, Int)) => B): B = root match {
     case Nil => container
-    case T(l, value, r) =>
+    case Tree(l, value, r) =>
       val leftContainer = foldLeftImpl(container, l)(op)
       foldLeftImpl(op(leftContainer, value), r)(op)
   }
@@ -240,24 +240,25 @@ final class MultiSet[A] private(private val root: N[A], val size: Int)(implicit 
 
 private object MultiSet {
 
-  sealed trait N[+A] {
+  sealed trait Node[+A] {
     def height: Int
   }
 
-  object N {
+  object Node {
 
     trait ParentHeight {
       val h: Int
     }
 
-    def unapply(arg: N[_])(implicit parentHeight: ParentHeight): Option[Int] = Some(arg.height - (parentHeight.h - 3))
+    def unapply(arg: Node[_])(implicit parentHeight: ParentHeight): Option[Int] =
+      Some(arg.height - (parentHeight.h - 3))
   }
 
-  case class T[A](l: N[A], value: (A, Int), r: N[A]) extends N[A] {
+  case class Tree[A](l: Node[A], value: (A, Int), r: Node[A]) extends Node[A] {
     override val height: Int = max(l.height, r.height) + 1
   }
 
-  case object Nil extends N[Nothing] {
+  case object Nil extends Node[Nothing] {
     override def height: Int = 0
   }
 
