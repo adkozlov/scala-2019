e3611856873d09dd0738e2f0e6e43725a1dcac91
Vladilav Yepifanov <evn1999@yandex.ru>
2019-12-14 17:22:44 +0300

    heh added tests to git

---
+++ src/test/scala/ru/spb/jvm/scala/treap/TreapTest.scala
@@ -0,0 +1,13 @@
+package ru.spb.jvm.scala.treap
+
+import org.scalatest.FunSuite
+import ru.spbau.jvm.scala.treap.Treap
+
+class TreapTest extends FunSuite {
+  private val seq = Seq(5, 1, 2, 3, 3, 5, 5)
+  private def uniqueSeq = seq.distinct
+
+  test("builds correctly") {
+
+  }
+}
