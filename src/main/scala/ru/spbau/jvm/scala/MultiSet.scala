9b38f397cd3a5442c8daf43fef29f7003ec3a68a
Irina Pluralia <irina-pluralia@rambler.ru>
2019-12-15 15:56:56 +0300

    Fix notes

--- src/main/scala/ru/spbau/jvm/scala/MultiSet.scala
+++ src/main/scala/ru/spbau/jvm/scala/MultiSet.scala
@@ -1,10 +1,8 @@
 package ru.spbau.jvm.scala
 
-import ru.spbau.jvm.scala.treap.{Leaf, Treap, Tree}
-
 object MultiSet {
   def empty[E]()(implicit ordering: Ordering[E]) =
-    new MultiSet[E](Leaf())
+    new MultiSet[E]
 
   def apply[E](elems: E*)(implicit ord: Ordering[E]): MultiSet[E] = {
     val ms = empty[E]()
@@ -15,64 +13,201 @@ object MultiSet {
   }
 }
 
-class MultiSet[E](private var tree: Tree[E, Int] = Leaf())(
-  implicit ord: Ordering[E]
-) {
+class MultiSet[K](implicit ord: Ordering[K]) {
+  private var tree: Tree = Leaf
 
-  def insert(elem: E): MultiSet[E] = insert(elem, 1)
+  def insert(elem: K): MultiSet[K] = insert(elem, 1)
 
-  def insert(elem: E, count: Int): MultiSet[E] = {
+  def insert(elem: K, count: Int): MultiSet[K] = {
     if (count > 0) {
-      val (opPri, newTree) = Treap.remove(elem, tree)
-      tree = Treap.insert(elem, count + opPri.getOrElse(0), newTree)
+      val (opPri, newTree) = remove(elem, tree)
+      tree = insert(Node(elem, count + opPri.getOrElse(0)), newTree)
     }
     this
   }
 
-  def remove(elem: E): MultiSet[E] = {
-    val (opPri, newTree) = Treap.remove(elem, tree)
+  def remove(elem: K): MultiSet[K] = {
+    val (opPri, newTree) = remove(elem, tree)
     val pri = opPri.map(x => x - 1).getOrElse(-1)
 
     if (pri > 0) {
-      tree = Treap.insert(elem, pri, newTree)
+      tree = insert(Node(elem, pri), newTree)
     }
     this
   }
 
-  def isEmpty: Boolean = tree == Leaf()
+  def isEmpty: Boolean = tree == Leaf
 
   def notEmpty: Boolean = !isEmpty
 
-  def countOf(elem: E): Int = {
-    Treap.getPriByKey(elem, tree).getOrElse(0)
+  def countOf(elem: K): Int = {
+    getCountByKey(elem, tree).getOrElse(0)
   }
 
-  def contains(elem: E): Boolean = countOf(elem) > 0
+  def contains(elem: K): Boolean = countOf(elem) > 0
 
-  def &(multiSet: MultiSet[E]): MultiSet[E] = {
-    val intersect = new MultiSet[E](Leaf())
-    Treap.foreach((key: E, pri: Int) => {
+  def foreach[R](f: K => R): Unit = foreachKey(f, tree)
+
+  def &(multiSet: MultiSet[K]): MultiSet[K] = {
+    val intersect = new MultiSet[K]
+    foreach((key: K, pri: Int) => {
       val count = pri.min(multiSet.countOf(key))
       intersect.insert(key, count)
     }, tree)
     intersect
   }
 
-  def |(multiSet: MultiSet[E]): MultiSet[E] = {
-    val union = new MultiSet[E](tree)
+  def |(multiSet: MultiSet[K]): MultiSet[K] = {
+    val union = new MultiSet[K]
+    foreach(key => union.insert(key, countOf(key)))
     multiSet.foreach(key => union.insert(key, multiSet.countOf(key)))
     union
   }
 
-  def foreach[R](f: E => R): Unit = Treap.foreachKey(f, tree)
+  def map[R](f: K => R)(implicit ord: Ordering[R]): MultiSet[R] = {
+    val set = new MultiSet[R]
+    foreach(key => set.insert(f(key), countOf(key)))
+    set
+  }
 
-  def map[R](f: E => R)(implicit ord: Ordering[R]): MultiSet[R] =
-    new MultiSet[R](Treap.map(f, tree))
+  def filter(p: K => Boolean): MultiSet[K] = {
+    val set = new MultiSet[K]
+    foreach(key => if (p(key)) set.insert(key, countOf(key)))
+    set
+  }
 
-  def filter(p: E => Boolean): MultiSet[E] =
-    new MultiSet[E](Treap.filter(p, tree))
+  override def toString: String = toString(tree)
 
-  override def toString: String = {
-    "[" ++ Treap.toString(tree) ++ "]"
+  override def equals(other: Any): Boolean = {
+    other match {
+      case tree: MultiSet[K] => {
+        for (key <- this) {
+          if (tree.countOf(key) != countOf(key)) {
+            return false
+          }
+        }
+        for (key <- tree) {
+          if (tree.countOf(key) != countOf(key)) {
+            return false
+          }
+        }
+        true
+      }
+      case _ => false
+    }
+  }
+
+  private abstract class Tree
+
+  private case class Node(key: K,
+                          pri: Int,
+                          left: Tree = Leaf,
+                          right: Tree = Leaf)
+      extends Tree
+
+  private case object Leaf extends Tree
+
+  private def split(splitKey: K, tree: Tree): (Tree, Tree) = {
+    tree match {
+      case Leaf => (Leaf, Leaf)
+      case Node(key: K, pri, left: Tree, right: Tree) =>
+        if (ord.lt(key, splitKey)) {
+          val (t1, t2) = split(splitKey, right)
+          (Node(key, pri, left, t1), t2)
+        } else {
+          val (t1, t2) = split(splitKey, left)
+          (t1, Node(key, pri, t2, right))
+        }
+    }
+  }
+
+  private def merge(tree1: Tree, tree2: Tree): Tree = {
+    (tree1, tree2) match {
+      case (Leaf, _) => tree2
+      case (_, Leaf) => tree1
+      case (Node(key1, pri1, left1, right1), Node(key2, pri2, left2, right2)) =>
+        if (pri1 < pri2) {
+          val t: Tree = merge(tree1, left2)
+          Node(key2, pri2, t, right2)
+        } else {
+          val t: Tree = merge(right1, tree2)
+          Node(key1, pri1, left1, t)
+        }
+    }
+  }
+
+  private def insert(node: Tree, tree: Tree): Tree = {
+    node match {
+      case Leaf => tree
+      case Node(key, _, _, _) =>
+        val (t1, t2) = split(key, tree)
+        merge(merge(t1, node), t2)
+    }
+  }
+
+  private def remove(removeKey: K, tree: Tree): (Option[Int], Tree) = {
+    tree match {
+      case Leaf => (None, Leaf)
+      case Node(key: K, pri, left, right) =>
+        if (ord.compare(key, removeKey) == 0) {
+          return (Some(pri), merge(left, right))
+        }
+
+        if (ord.lt(key, removeKey)) {
+          val (opPri, newRight) = remove(removeKey, right)
+          (opPri, Node(key, pri, left, newRight))
+        } else {
+          val (opPri, newLeft) = remove(removeKey, left)
+          (opPri, Node(key, pri, newLeft, right))
+        }
+    }
+  }
+
+  @scala.annotation.tailrec
+  private def getCountByKey(searchKey: K, tree: Tree): Option[Int] = {
+    tree match {
+      case Leaf => None
+      case Node(key: K, pri, left, right) =>
+        if (key == searchKey) {
+          return Some(pri)
+        }
+        getCountByKey(searchKey, if (ord.lt(key, searchKey)) right else left)
+    }
+  }
+
+  private def toString(tree: Tree): String = {
+    tree match {
+      case Leaf => "[]"
+      case Node(key, pri, left, right) =>
+        var (leftStr, rightStr) =
+          (toString(left).dropRight(1), toString(right).drop(1))
+        if (leftStr.length > 1) {
+          leftStr += ", "
+        }
+        if (rightStr.length > 1) {
+          rightStr = ", " ++ rightStr
+        }
+        leftStr + key + " -> " + pri + rightStr
+    }
+  }
+
+  private def foreach[R](f: (K, Int) => R, tree: Tree): Unit = {
+    tree match {
+      case Leaf =>
+      case Node(key, pri, left, right) =>
+        foreach(f, left)
+        f(key, pri)
+        foreach(f, right)
+    }
+  }
+
+  private def foreachKey[R, P](f: K => R, tree: Tree): Unit = {
+    tree match {
+      case Leaf =>
+      case Node(key, _, left, right) =>
+        foreachKey(f, left)
+        f(key)
+        foreachKey(f, right)
+    }
   }
 }
--- src/test/scala/ru/spbau/jvm/scala/MultiSetTest.scala
+++ src/test/scala/ru/spbau/jvm/scala/MultiSetTest.scala
@@ -5,7 +5,25 @@ import org.scalatest.{FlatSpec, Matchers}
 class MultiSetTest extends FlatSpec with Matchers {
   "fillFromList" should "ok" in {
     val multiSet = MultiSet(4, 8, 15, 16, 23, 42, 42)
-    multiSet.toString should be("[ 4->1  8->1  15->1  16->1  23->1  42->2 ]")
+    multiSet.toString should be(
+      "[4 -> 1, 8 -> 1, 15 -> 1, 16 -> 1, 23 -> 1, 42 -> 2]"
+    )
+  }
+
+  "equal" should "ok" in {
+    val multiSet = MultiSet(4, 8, 15, 16, 23, 42, 42)
+    multiSet should be(MultiSet(4, 8, 15, 16, 23, 42, 42))
+    multiSet should be(MultiSet(42, 4, 15, 8, 23, 42, 16))
+  }
+
+  "remove" should "ok" in {
+    var multiSet = MultiSet(4, 8, 15, 16, 23, 42, 42, 42)
+
+    multiSet = multiSet.remove(42)
+    multiSet should be(MultiSet(4, 8, 15, 16, 23, 42, 42))
+
+    multiSet = multiSet.remove(42)
+    multiSet should be(MultiSet(4, 8, 15, 16, 23, 42))
   }
 
   "countOf" should "ok" in {
@@ -15,47 +33,41 @@ class MultiSetTest extends FlatSpec with Matchers {
     multiSet.countOf(7) should be(0)
   }
 
-  "map" should "ok" in {
-    val multiSet = MultiSet(1, 4, 1, 3, 3, 3)
-    multiSet.toString should be("[ 1->2  3->3  4->1 ]")
-
-    multiSet.map(x => x + 1).toString should be("[ 2->2  4->3  5->1 ]")
-
-    multiSet.map(x => if (x == 1) 5 else x).toString should be(
-      "[ 3->3  5->2  4->1 ]"
-    )
-  }
-
-  "filter" should "ok" in {
-    MultiSet(1, 4, 1, 3, 3, 3).filter(x => x % 2 == 0).toString should be(
-      "[ 4->1 ]"
-    )
-
-    MultiSet(4, 8, 15, 16, 23, 42,
-      42).filter(x => x % 2 == 0).toString should be(
-      "[ 4->1  8->1  16->1  42->2 ]"
-    )
-  }
-
   "intersect(&)" should "ok" in {
     val multiSet = MultiSet(4, 8, 15, 16, 23, 42, 42)
     val ms42 = MultiSet(42)
-    (multiSet & ms42).toString should be("[ 42->1 ]")
-    (ms42 & multiSet).toString should be("[ 42->1 ]")
+    (multiSet & ms42) should be(MultiSet(42))
+    (ms42 & multiSet) should be(MultiSet(42))
 
     val ms42x2 = MultiSet(42, 42)
-    (multiSet & ms42x2).toString should be("[ 42->2 ]")
-    (ms42x2 & multiSet).toString should be("[ 42->2 ]")
+    (multiSet & ms42x2) should be(MultiSet(42, 42))
+    (ms42x2 & multiSet) should be(MultiSet(42, 42))
   }
 
   "union(|)" should "ok" in {
     val multiSet = MultiSet(4, 8, 15, 16, 23, 42, 42)
     val ms108 = MultiSet(108)
-    (multiSet | ms108).toString should be(
-      "[ 4->1  8->1  15->1  16->1  23->1  42->2  108->1 ]"
+    (multiSet | ms108) should be(MultiSet(4, 8, 15, 16, 23, 42, 42, 108))
+
+    (ms108 | multiSet) should be(MultiSet(4, 8, 15, 16, 23, 42, 42, 108))
+  }
+
+  "map" should "ok" in {
+    val multiSet = MultiSet(1, 4, 1, 3, 3, 3)
+    multiSet should be(MultiSet(1, 1, 3, 3, 3, 4))
+
+    multiSet.map(x => x + 1) should be(MultiSet(2, 2, 4, 4, 4, 5))
+
+    multiSet.map(x => if (x == 1) 5 else x) should be(
+      MultiSet(3, 3, 3, 4, 5, 5)
     )
-    (ms108 | multiSet).toString should be(
-      "[ 4->1  8->1  15->1  16->1  23->1  42->2  108->1 ]"
+  }
+
+  "filter" should "ok" in {
+    MultiSet(1, 4, 1, 3, 3, 3).filter(x => x % 2 == 0) should be(MultiSet(4))
+
+    MultiSet(4, 8, 15, 16, 23, 42, 42).filter(x => x % 2 == 0) should be(
+      MultiSet(4, 8, 16, 42, 42)
     )
   }
 }
